ce4dcfc96cab659c2f356c07193423720441f762
박의인 <fate7902@naver.com>
2023-04-20 18:51:41 +0900

    baekjoon 1 question

---
+++ baekjoon/10001 ~ 15000/10953.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string str;
+        cin >> str;
+        cout << (str[0] - '0') + (str[2] - '0') << "\n";
+    }
+}
