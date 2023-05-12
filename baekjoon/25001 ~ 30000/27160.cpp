c77407d770bc0a2f7db29e9d90392c0b4aa5e6c6
박의인 <fate7902@naver.com>
2023-05-12 19:40:58 +0900

    baekjoon 1 question

---
+++ baekjoon/25001 ~ 30000/27160.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    map<string, int> map;
+    while (n--) {
+        string fruit;
+        int num;
+        cin >> fruit >> num;
+        map[fruit] += num;
+    }
+
+    for (auto& m : map) {
+        if (m.second == 5) {
+            cout << "YES";
+            return 0;
+        }
+    }
+    cout << "NO";
+}
