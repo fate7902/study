43e67bd32e48845e5821b38e88ff111248798931
박의인 <fate7902@naver.com>
2023-06-14 12:39:49 +0900

    programmers Lv.0 14 questions

---
+++ Programmers/Lv.0/날짜 비교하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> date1, vector<int> date2) {    
+    return [&]{
+        for(int i = 0; i < 3; ++i){
+            if(date1[i] > date2[i]) return 0;
+            else if(date1[i] < date2[i]) return 1;
+        }
+        return 0;
+    }();
+}
---
+++ Programmers/Lv.0/문자열 반복해서 출력하기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    int n;
+    cin >> str >> n;
+    while(n--) cout << str;
+}
